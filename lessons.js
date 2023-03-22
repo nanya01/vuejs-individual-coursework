033341aec96f4e091026e80725145e7bed7f7419
Peter <peterahunanya79@gmail.com>
2023-03-22 09:44:04 +0100

    added-lessons-to-lessons.js

---
+++ lessons.js
@@ -0,0 +1,96 @@
+let lessons = [
+    {
+         "id": 1,
+        "Subject": "Math",
+        "Location": "Dubai",
+        "Price": 2.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+    {
+        "id": 2,
+        "Subject": "English",
+        "Location": "London",
+        "Price": 1.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\math.jpg",
+        "icon":""
+    },
+    {
+        "id": 3,
+        "Subject": "vue.js",
+        "Location": "Mauritus",
+        "Price": 6.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\math.jpg",
+        "icon":""
+    },
+    {
+        "id": 4,
+        "Subject": "chemistry",
+        "Location": "Nigeria",
+        "Price": 7.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+        
+    },
+    {
+        "id": 5,
+        "Subject": "Biology",
+        "Location": "Dubai",
+        "Price": 9.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+
+    {
+        "id": 6,
+        "Subject": "Physics",
+        "Location": "Spain",
+        "Price": 4.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+    {
+        "id": 7,
+        "Subject": "node.js",
+        "Location": "Italy",
+        "Price": 50.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+    {
+        "id": 8,
+        "Subject": "Flutter",
+        "Location": "USA",
+        "Price": 52.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+    {
+        "id": 9,
+        "Subject": "kotlin",
+        "Location": "Germany",
+        "Price": 8.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+    {
+        "id": 10,
+        "Subject": "python",
+        "Location": "Austria",
+        "Price": 6.99,
+        "Space": 5,
+        "image": "week 5\individual_coursework\antoine-dautry-05A-kdOH6Hw-unsplash.jpg",
+        "icon":""
+    },
+];
+
+
